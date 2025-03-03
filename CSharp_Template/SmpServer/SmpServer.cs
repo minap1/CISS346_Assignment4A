f2430965ac6459ca5734caa9bbd41b1b368dba9f
stillTheOnlySwissArmy <eric.tootill@gmail.com>
2025-03-03 11:28:24 -0800

    Added C-sharp files

---
+++ CSharp_Template/SmpServer/SmpServer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmpServer
+{
+    public partial class FormSmpServer : Form
+    {
+        public FormSmpServer()
+        {
+            InitializeComponent();
+        }
+    }
+}
