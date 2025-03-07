aafe90235658b979dd7adb3466d22bc2c8b71c8b
Eric Tootill <eric.tootill@gmail.com>
2025-03-07 10:52:57 -0800

    more fxml fun

--- Assignment04A-SourceCode/demo/src/main/kotlin/org/example/ConsumerFXForm/consumerController.java
+++ Assignment04A-SourceCode/demo/src/main/kotlin/org/example/ConsumerFXForm/consumerController.java
@@ -1,4 +1,17 @@
 package org.example.ConsumerFXForm;
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class consumerController {
+    @FXML public RadioButton low;
+    @FXML public RadioButton medium;
+    @FXML public RadioButton latest;
+    @FXML public RadioButton high;
+    @FXML public RadioButton consume;
+    @FXML public RadioButton priority;
+    @FXML public TextArea reply;
+    @FXML public TextField address;
+    @FXML public TextField port;
 }
--- Assignment04A-SourceCode/demo/src/main/kotlin/org/example/ProducerFXForm/producerController.java
+++ Assignment04A-SourceCode/demo/src/main/kotlin/org/example/ProducerFXForm/producerController.java
@@ -2,21 +2,27 @@ package org.example.ProducerFXForm;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
 public class producerController {
 
-    @FXML TextArea input;
-    @FXML RadioButton rb1;
-    @FXML RadioButton rb2;
-    @FXML RadioButton rb3;
+    @FXML public Button browse;
+    @FXML public TextField filename;
+    @FXML public Button clear_file;
+    @FXML public TextArea serverReply;
+    @FXML public TextArea input;
+    @FXML public RadioButton rb1;
+    @FXML public RadioButton rb2;
+    @FXML public RadioButton rb3;
+    @FXML public TextField port;
+    @FXML public TextField address;
+    @FXML public CheckBox clearInput;
 
     final ToggleGroup group = new ToggleGroup();
+
+
 
     @FXML
     public void initialize() {
